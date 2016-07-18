2f440934d3bc0404d832686912cc047bb2646f10
Jonathan Campbell <jonathan@castus.tv>
2016-07-17 23:07:21 -0700

    more

---
+++ cpudec86.cpp
@@ -0,0 +1,78 @@
+
+#ifdef HAVE_CONFIG_H
+# include "config.h" // must be first
+#endif
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+
+#include "dosboxxr/lib/cpu/x86ModRegRm.h"
+#include "dosboxxr/lib/cpu/memreftypes.h"
+
+unsigned char*          exe_image = NULL;
+unsigned char*          exe_image_fence = NULL;
+
+int main(int argc,char **argv) {
+    const char *src_file = NULL,*arg;
+    off_t file_size;
+    int fd;
+    int i;
+
+    for (i=1;i < argc;) {
+        arg = argv[i++];
+
+        if (*arg == '-') {
+            do { arg++; } while (*arg == '-');
+
+            if (!strcmp(arg,"i")) {
+                src_file = argv[i++];
+            }
+            else {
+                fprintf(stderr,"Unknown sw %s\n",arg);
+                return 1;
+            }
+        }
+        else {
+            fprintf(stderr,"Unexpected arg %s\n",arg);
+            return 1;
+        }
+    }
+
+    if (src_file == NULL) {
+        fprintf(stderr,"Must specify file\n");
+        return 1;
+    }
+
+    fd = open(src_file,O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr,"Cannot open input file\n");
+        return 1;
+    }
+    file_size = lseek(fd,0,SEEK_END);
+    if (lseek(fd,0,SEEK_SET) != (off_t)0 || file_size <= (off_t)0 || file_size > (off_t)0xFFFFU) {
+        fprintf(stderr,"Bad file size\n");
+        return 1;
+    }
+
+    exe_image = (unsigned char*)malloc((size_t)file_size);
+    if (exe_image == NULL) {
+        fprintf(stderr,"Cannot alloc EXE image\n");
+        return 1;
+    }
+    if (read(fd,exe_image,file_size) != (int)file_size) {
+        fprintf(stderr,"Cannot read EXE image\n");
+        return 1;
+    }
+    exe_image_fence = exe_image + (size_t)file_size;
+
+    free(exe_image);
+    exe_image = NULL;
+    exe_image_fence = NULL;
+    close(fd);
+    return 0;
+}
+
