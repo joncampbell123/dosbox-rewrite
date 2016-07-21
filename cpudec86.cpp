f66b248b55bcea19a93716424f1fd2463d6a98d7
Jonathan Campbell <jonathan@castus.tv>
2016-07-20 23:10:57 -0700

    more

--- cpudec86.cpp
+++ cpudec86.cpp
@@ -55,32 +55,7 @@ static inline uint32_t IPFDW(void) {
     return r;
 }
 
-static inline int32_t IPFBsigned(void) {
-    return (int32_t)((int8_t)IPFB());
-}
-
-static inline int32_t IPFWsigned(void) {
-    return (int32_t)((int16_t)IPFW());
-}
-
-static inline void IPFModRegRm(x86ModRegRm &m) {
-    m.byte = IPFB();
-}
-
-// given mod/reg/rm fetch displacement (16-bit code)
-static inline x86_offset_t IPFmrmdisplace16(x86ModRegRm &mrm) {
-    switch (mrm.mod()) {
-        case 0:
-            if (mrm.rm() == 6) return IPFW();
-            return 0;
-        case 1:
-            return IPFBsigned();
-        case 2:
-            return IPFW();
-        case 3:
-            return 0;
-    };
-}
+#include "dosboxxr/lib/cpu/ipdec_pre_core.h"
 
 //// CPU CORE
 #define DECOMPILEMODE
