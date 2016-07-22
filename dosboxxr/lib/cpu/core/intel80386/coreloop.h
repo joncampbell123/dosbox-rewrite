fb3e9ee7d970bbba823389ed1bbc184f85974666
Jonathan Campbell <jonathan@castus.tv>
2016-07-22 13:54:35 -0700

    XOR i386 32-bit

--- dosboxxr/lib/cpu/core/intel80386/coreloop.h
+++ dosboxxr/lib/cpu/core/intel80386/coreloop.h
@@ -305,31 +305,31 @@
 #endif
                     break;
                 case 0x30: // XOR r/m,reg byte size
-                    mrm.set(IPFB());
-                    disp = IPFmrmdisplace16(/*&*/mrm);
+                    IPDec386Load_MRM_SIB(/*&*/mrm,/*&*/sib,/*&*/disp,addr32);
 #ifdef DECOMPILEMODE
-                    w += snprintf(w,(size_t)(wf-w),"XORb %s,%s",IPDecPrint16(mrm,disp,1),CPUregs8[mrm.reg()]);
+                    w += snprintf(w,(size_t)(wf-w),"XOR%s %s,%s",sizesuffix[1],
+                        IPDecPrint386(mrm,sib,disp,1,addr32),CPUregsN[1][mrm.reg()]);
 #endif
                     break;
                 case 0x31: // XOR r/m,reg word size
-                    mrm.set(IPFB());
-                    disp = IPFmrmdisplace16(/*&*/mrm);
+                    IPDec386Load_MRM_SIB(/*&*/mrm,/*&*/sib,/*&*/disp,addr32);
 #ifdef DECOMPILEMODE
-                    w += snprintf(w,(size_t)(wf-w),"XORw %s,%s",IPDecPrint16(mrm,disp,2),CPUregs16[mrm.reg()]);
+                    w += snprintf(w,(size_t)(wf-w),"XOR%s %s,%s",sizesuffix[COREWORDSIZE],
+                        IPDecPrint386(mrm,sib,disp,COREWORDSIZE,addr32),CPUregsN[COREWORDSIZE][mrm.reg()]);
 #endif
                     break;
                 case 0x32: // XOR reg,r/m byte size
-                    mrm.set(IPFB());
-                    disp = IPFmrmdisplace16(/*&*/mrm);
+                    IPDec386Load_MRM_SIB(/*&*/mrm,/*&*/sib,/*&*/disp,addr32);
 #ifdef DECOMPILEMODE
-                    w += snprintf(w,(size_t)(wf-w),"XORb %s,%s",CPUregs8[mrm.reg()],IPDecPrint16(mrm,disp,1));
+                    w += snprintf(w,(size_t)(wf-w),"XOR%s %s,%s",sizesuffix[1],
+                        CPUregsN[1][mrm.reg()],IPDecPrint386(mrm,sib,disp,1,addr32));
 #endif
                     break;
                 case 0x33: // XOR reg,r/m word size
-                    mrm.set(IPFB());
-                    disp = IPFmrmdisplace16(/*&*/mrm);
+                    IPDec386Load_MRM_SIB(/*&*/mrm,/*&*/sib,/*&*/disp,addr32);
 #ifdef DECOMPILEMODE
-                    w += snprintf(w,(size_t)(wf-w),"XORw %s,%s",CPUregs16[mrm.reg()],IPDecPrint16(mrm,disp,2));
+                    w += snprintf(w,(size_t)(wf-w),"XOR%s %s,%s",sizesuffix[COREWORDSIZE],
+                        CPUregsN[COREWORDSIZE][mrm.reg()],IPDecPrint386(mrm,sib,disp,COREWORDSIZE,addr32));
 #endif
                     break;
                 case 0x34: // XOR AL,imm8
@@ -338,10 +338,12 @@
                     w += snprintf(w,(size_t)(wf-w),"XORb AL,%02Xh",v8);
 #endif
                     break;
-                case 0x35: // XOR AX,imm16
-                    v16 = IPFW();
+                case 0x35: // XOR AX,immword
+                    // Your C++ compiler is smart enough to optimize out the conditional here, right?
+                    // COREWORDSIZE is a #define and therefore the ternary statements are predictable and can be reduced to one or the other for 16/32-bit modes.
+                    v32 = COREWORDSIZE == 4 ? IPFDW() : IPFW();
 #ifdef DECOMPILEMODE
-                    w += snprintf(w,(size_t)(wf-w),"XORw AX,%04Xh",v16);
+                    w += snprintf(w,(size_t)(wf-w),"XOR%c %s,%08lXh",COREWORDSIZE == 4 ? 'd' : 'w',CPUregsN[COREWORDSIZE][/*AX*/0],(unsigned long)v32);
 #endif
                     break;
                 case 0x36:
