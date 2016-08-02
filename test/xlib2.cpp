06258a52f6ec59869c7ae6205e056447ff439ce9
Jonathan Campbell <jonathan@castus.tv>
2016-08-02 12:25:19 -0700

    separate out nr_wfpack

--- test/xlib2.cpp
+++ test/xlib2.cpp
@@ -23,6 +23,7 @@
 #include <algorithm>
 
 #include "dosboxxr/lib/hostcpudetect/caps.h"
+#include "dosboxxr/lib/util/nr_wfpack.h"
 
 #if HAVE_CPU_MMX
 # include <mmintrin.h>
@@ -295,83 +296,6 @@ void src_bitmap_render() {
             x_image->bits_per_pixel);
     }
 }
-
-// wftype: must match the size of a CPU register to do fixed point math without shifting.
-// the rationale is that, from experience with x86 processors past the Pentium III,
-// processors are faster at addition, subtraction, and multiplication than they are with
-// logical operators like AND, OR, and shift. therefore, we can speed up horizontal
-// interpolation by managing the whole + fractional parts in a way that eliminates
-// the shift operation when reading the whole part. But to accomplish this, we have to
-// define the base datatype as one the exact width of a CPU register.
-#if defined(__i386__) // x86
-typedef uint32_t			nr_wftype;	// datatype the size of a CPU register
-#elif defined(__x86_64__) // x86_64
-typedef uint64_t			nr_wftype;	// datatype the size of a CPU register
-#else
-typedef unsigned int			nr_wftype;	// datatype the size of a CPU register
-#endif
-
-// number of bits in wftype
-static const unsigned char				nr_wfbits = (sizeof(nr_wftype) * 8);
-
-// what to mask the wftype by to obtain the upper 8 bits (i.e. checking whether the fractional part is effectively zero)
-static const nr_wftype					nr_top8bitmask = ((nr_wftype)0xFF << (nr_wftype)(nr_wfbits - 8));
-
-// what to mask the fraction by if comparing to "close enough" to 1:1
-static const nr_wftype					nr_wfscsigbits = ((nr_wftype)0xFFFF << (nr_wftype)(nr_wfbits - 16));
-static const nr_wftype					nr_wfscsigbits_add = ((nr_wftype)1 << (nr_wftype)(nr_wfbits - (16 + 1)));
-static const nr_wftype					nr_wfhalfmax = ((nr_wftype)1 << (nr_wftype)(nr_wfbits - 1));
-
-// wftype pack to contain whole & fractional parts
-struct nr_wfpack {
-public:
-	nr_wftype			w,f;
-public:
-	// add to THIS struct another struct. inline for performance!
-	inline void add(const struct nr_wfpack &b) {
-#if defined(__x86_64__) && defined(nr_ALLOW_ASM) // x86_64 optimized version because GCC can't figure out what we're trying to do with carry
-		// f += b.f
-		// w += b.w + carry
-		__asm__ (	"addq	%2,%0\n"
-				"adcq	%3,%1\n" :
-				/* outputs */ "+rme" (f), "+rme" (w) : /* +rme to mean we read/modify/write the outputs */
-				/* inputs */ "ri" (b.f), "ri" (b.w) : /* make input params registers, add cannot take two memory operands */
-				/* clobbered */ "cc" /* modifies flags */);
-#elif defined(__i386__) && defined(nr_ALLOW_ASM) // i686 optimized version because GCC can't figure out what we're trying to do with carry
-		// f += b.f
-		// w += b.w + carry
-		__asm__ (	"addl	%2,%0\n"
-				"adcl	%3,%1\n" :
-				/* outputs */ "+rme" (f), "+rme" (w) : /* +rme to mean we read/modify/write the outputs */
-				/* inputs */ "ri" (b.f), "ri" (b.w) : /* make input params registers, add cannot take two memory operands */
-				/* clobbered */ "cc" /* modifies flags */);
-#else // generic version. may be slower than optimized because GCC probably won't figure out we want i686 equiv. of add eax,ebx followed by adc ecx,edx, the carry part)
-		nr_wftype ov = f;
-		w += b.w+(((f += b.f) < ov)?1:0);
-		// what we want: add fractions. add whole part, with carry from fractions.
-#endif
-	}
-	// C++ convenience for add()
-	inline struct nr_wfpack& operator+=(const struct nr_wfpack &b) {
-		add(b);
-		return *this;
-	}
-	inline struct nr_wfpack operator+(const struct nr_wfpack &b) {
-		struct nr_wfpack r = *this;
-		return (r += b);
-	}
-
-	// inline for performance!
-	inline unsigned int mul_h(const unsigned int b,const unsigned int a) {
-		// NTS: i686 handwritten version is slower than the generic
-#if 0
-#else
-		// NTS: Despite the fact modern CPUs are faster at ADD than SHR, this is faster
-		//      than typecasting to long long then shifting down. By quite a bit.
-		return a + (unsigned int)((((int)b - (int)a) * (f >> (nr_wfbits - 8))) >> 8);
-#endif
-	}
-};
 
 void render_scale_from_sd(struct nr_wfpack &sy,const uint32_t dh,const uint32_t sh) {
 	uint64_t t;
