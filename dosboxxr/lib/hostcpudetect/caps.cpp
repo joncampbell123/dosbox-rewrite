74d7633cfbd4559daf4f0a1a259754fd815e0e3c
Jonathan Campbell <jonathan@castus.tv>
2016-08-06 23:11:42 -0700

    MinGW/Windows: Just because the CPU supports SSE/AVX doesn't mean the OS has enabled the instruction set. Windows has the problem where MinGW/GCC CPU detection will report SSE/AVX on downlevel OSes but using the instruction sets will crash. For Windows targets this code adds checking to see if the OS kernel supports the instruction set.
    Noted cases:
     - Windows XP on a CPU with AVX instructions. Builtin CPU detection will
       report AVX but Windows XP doesn't know about AVX and using it will
       cause our program to fault.
    
     - Windows 98 (or earlier) on a CPU with SSE instructions. SSE support
       wasn't added until Windows 98 SE. Coindicentally,
       IsProcessorFeaturePresent appears in 98SE as well, and even though
       the function doesn't report SSE reliably, it is a sign nonetheless
       the OS kernel supports and enables SSE extensions.
    
    This code is Windows specific since other OSes like Linux and Mac OS X
    tend to list CPU extensions if and only if supported by the kernel AND
    detected by the kernel.

--- dosboxxr/lib/hostcpudetect/caps.cpp
+++ dosboxxr/lib/hostcpudetect/caps.cpp
@@ -3,6 +3,15 @@
 # include "config.h" // must be first
 #endif
 
+#if IS_WINDOWS
+# include <windows.h>
+
+// dammit MinGW
+# ifndef PF_SSE3_INSTRUCTIONS_AVAILABLE
+#  define PF_SSE3_INSTRUCTIONS_AVAILABLE 13
+# endif
+#endif
+
 #include <sys/types.h>
 #if IS_MAC_OSX
 # include <sys/sysctl.h>
@@ -23,6 +32,47 @@
 
 // this is where this is defined
 struct HostCPUCaps hostCPUcaps;
+
+#if IS_WINDOWS
+void HostCPUCaps::x86_win32_check_os_support(void) {
+    /* When targeting Windows, we need to consider that MinGW/GCC uses CPUID to detect capabilities.
+       The problem with x86 is that while CPUID can report SSE/AVX is available, it doesn't mean the
+       OS has enabled it. Consider the following cases where we will crash if we blindly trust the
+       CPU capability detection of GCC builtins:
+
+       Windows XP on a CPU with AVX/AVX2 instructions (Windows XP does not support or enable AVX)
+       Windows 98 and earlier on a CPU with SSE instructions (SSE support was not added until Windows 98 SE).
+
+       The way to detect whether the OS has actually enabled it, is the IsProcessorFeaturePresent() API function.
+       The OS will tell us whether SSE/AVX is actually available.
+
+       Note that IsProcessorFeaturePresent() does not appear until Windows 98 SE and Windows 2000 (when SSE support appeared).
+       That is the reason why we use GetProcAddress instead of assuming the symbol is present.
+       If that symbol does not exist in KERNEL32.DLL, then we assume that anything other than MMX support does not exist in the OS. */
+    BOOL (WINAPI *__IsProcessorFeaturePresent)(DWORD ProcessorFeature) =
+        (BOOL (WINAPI *)(DWORD))GetProcAddress(GetModuleHandle("KERNEL32.DLL"),"IsProcessorFeaturePresent");
+
+    if (__IsProcessorFeaturePresent != NULL) {
+        // *SIGH* Guess what... Windows 98 SE always returns FALSE for PF_XMMI_INSTRUCTIONS_AVAILABLE.
+        // So the best we can go on is to assume that if this entry point exists, that we're running under a version
+        // of Windows that supports SSE even if the function does fuck all to help us.
+
+        // Here's another one: IsProcessorFeaturePresent() cannot be used to detect AVX support either (added somewhere around Windows 7 SP1).
+        // But, they added a KERNEL32.DLL API for it, and once again, we use that to detect OS support for AVX/AVX2 instructions.
+        if (GetProcAddress(GetModuleHandle("KERNEL32.DLL"),"GetEnabledXStateFeatures") == NULL)
+            avx = avx2 = false; // if it doesn't exist, then we can't use AVX and AVX2 instructions.
+    }
+    else {
+        fprintf(stderr,"IsProcessorFeaturePresent doesn't exist\n");
+        // IsProcessorFeaturePresent() doesn't exist. That means we're running on a version of Windows that pre-dates SSE extensions:
+        //   Windows 95, 95 OSR2
+        //   Windows 98 (first edition)
+        //   Windows NT 4.x and earlier
+        sse = sse2 = sse3 = ssse3 = false;
+        avx = avx2 = false;
+    }
+}
+#endif
 
 void HostCPUCaps::detect() {
 #if defined(__arm__)
@@ -103,6 +153,10 @@ void HostCPUCaps::detect() {
     ssse3 = __builtin_cpu_supports("ssse3");
     avx = __builtin_cpu_supports("avx");
     avx2 = __builtin_cpu_supports("avx2");
+
+#  if IS_WINDOWS
+    x86_win32_check_os_support();
+#  endif
 # elif IS_MAC_OSX
 /*------------------ x86/x86_64 Mac OS X has a way to query CPU features -------*/
     char line[4096],*value;
--- dosboxxr/lib/hostcpudetect/caps.h
+++ dosboxxr/lib/hostcpudetect/caps.h
@@ -12,6 +12,8 @@ struct HostCPUCaps {
     bool                        avx2;
 
     HostCPUCaps() : detect_method(NULL), mmx(0), sse(0), sse2(0), sse3(0), ssse3(0), avx(0), avx2(0) { }
+
+    void                        x86_win32_check_os_support(void);
 #elif defined(__arm__)
     bool                        neon;
     HostCPUCaps() : detect_method(NULL), neon(0) { }
