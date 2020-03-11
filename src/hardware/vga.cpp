3205126ece2a57e3d533efe99295281123e091b5
Jonathan Campbell <jonathan@castus.tv>
2020-03-10 21:17:15 -0700

    more

--- src/hardware/vga.cpp
+++ src/hardware/vga.cpp
@@ -127,12 +127,13 @@
 #include "setup.h"
 #include "timer.h"
 #include "mem.h"
+#include "cpu.h"
 #include "util_units.h"
 #include "control.h"
 #include "mixer.h"
 #include "menu.h"
-#include "mem.h"
 #include "render.h"
+#include "paging.h"
 
 #include <string.h>
 #include <stdlib.h>
@@ -144,6 +145,8 @@
 #endif
 
 #define crtc(blah) vga.crtc.blah
+
+#define VGA_PAGE_B8		(0xB8000/4096)
 
 using namespace std;
 
@@ -404,5 +407,171 @@ void VGA_Init() {
     VGA_DAC_UpdateColorPalette();
 
     AddVMEventFunction(VM_EVENT_RESET,AddVMEventFunctionFuncPair(VGA_Reset));
+}
+
+static inline Bitu VGA_Generic_Read_Handler(PhysPt planeaddr,PhysPt rawaddr,unsigned char plane) {
+    const unsigned char hobit_n = (vga.seq.memory_mode&2/*Extended Memory*/) ? 16u : 14u;
+
+    /* Sequencer Memory Mode Register (04h)
+     * bits[3:3] = Chain 4 enable
+     * bits[2:2] = Odd/Even Host Memory Write Addressing Disable
+     * bits[1:1] = Extended memory (when EGA cards have > 64KB of RAM)
+     * 
+     * NTS: Real hardware experience says that despite the name, the Odd/Even bit affects reading as well */
+    plane = (plane & ~1u) + (rawaddr & 1u);
+
+    /* Graphics Controller: Miscellaneous Graphics Register register (06h)
+     * bits[3:2] = memory map select
+     * bits[1:1] = Chain Odd/Even Enable
+     * bits[0:0] = Alphanumeric Mode Disable
+     *
+     * http://www.osdever.net/FreeVGA/vga/graphreg.htm
+     *
+     * When enabled, address bit A0 (bit 0) becomes bit 0 of the plane index.
+     * Then when addressing VRAM A0 is replaced by a "higher order bit", which is
+     * probably A14 or A16 depending on Extended Memory bit 1 in Sequencer register 04h memory mode */
+    {/* Odd/Even enable */
+        const PhysPt mask = (vga.config.compatible_chain4 ? 0u : ~0xFFFFu) + (1u << hobit_n) - 2u;
+        const PhysPt hobit = (planeaddr >> hobit_n) & 1u;
+        /* 1 << 14 =     0x4000
+         * 1 << 14 - 1 = 0x3FFF
+         * 1 << 14 - 2 = 0x3FFE
+         * The point is to mask upper bit AND the LSB */
+        planeaddr = (planeaddr & mask & (vga.mem.memmask >> 2u)) + hobit;
+    }
+
+    vga.latch.d=((Bit32u*)vga.mem.linear)[planeaddr];
+    return (vga.latch.b[plane]);
+}
+
+template <const bool chained> static inline void VGA_Generic_Write_Handler(PhysPt planeaddr,PhysPt rawaddr,Bit8u val) {
+    const unsigned char hobit_n = (vga.seq.memory_mode&2/*Extended Memory*/) ? 16u : 14u;
+    Bit32u mask = 0xFFFF;
+
+    /* Sequencer Memory Mode Register (04h)
+     * bits[3:3] = Chain 4 enable
+     * bits[2:2] = Odd/Even Host Memory Write Addressing Disable
+     * bits[1:1] = Extended memory (when EGA cards have > 64KB of RAM)
+     * 
+     * NTS: Real hardware experience says that despite the name, the Odd/Even bit affects reading as well */
+    mask &= 0xFF00FFu << ((rawaddr & 1u) * 8u);
+
+    /* Graphics Controller: Miscellaneous Graphics Register register (06h)
+     * bits[3:2] = memory map select
+     * bits[1:1] = Chain Odd/Even Enable
+     * bits[0:0] = Alphanumeric Mode Disable
+     *
+     * http://www.osdever.net/FreeVGA/vga/graphreg.htm
+     *
+     * When enabled, address bit A0 (bit 0) becomes bit 0 of the plane index.
+     * Then when addressing VRAM A0 is replaced by a "higher order bit", which is
+     * probably A14 or A16 depending on Extended Memory bit 1 in Sequencer register 04h memory mode */
+    {/* Odd/Even enable */
+        const PhysPt mask = (vga.config.compatible_chain4 ? 0u : ~0xFFFFu) + (1u << hobit_n) - 2u;
+        const PhysPt hobit = (planeaddr >> hobit_n) & 1u;
+        /* 1 << 14 =     0x4000
+         * 1 << 14 - 1 = 0x3FFF
+         * 1 << 14 - 2 = 0x3FFE
+         * The point is to mask upper bit AND the LSB */
+        planeaddr = (planeaddr & mask & (vga.mem.memmask >> 2u)) + hobit;
+    }
+
+    VGA_Latch pixels;
+
+    pixels.d =((Bit32u*)vga.mem.linear)[planeaddr];
+    pixels.d&=~mask;
+    pixels.d|=(ExpandTable[val] & mask);
+
+    ((Bit32u*)vga.mem.linear)[planeaddr]=pixels.d;
+}
+
+class VGA_UnchainedVGA_Handler : public PageHandler {
+public:
+	Bitu readHandler(PhysPt start) {
+        return VGA_Generic_Read_Handler(start, start, vga.config.read_map_select);
+	}
+public:
+	Bit8u readb(PhysPt addr) {
+		addr = PAGING_GetPhysicalAddress(addr) & vga.mem.memmask;
+		return (Bit8u)readHandler(addr);
+	}
+	Bit16u readw(PhysPt addr) {
+		addr = PAGING_GetPhysicalAddress(addr) & vga.mem.memmask;
+		Bit16u ret = (Bit16u)(readHandler(addr+0) << 0);
+		ret     |= (readHandler(addr+1) << 8);
+		return  ret;
+	}
+	Bit32u readd(PhysPt addr) {
+		addr = PAGING_GetPhysicalAddress(addr) & vga.mem.memmask;
+		Bit32u ret = (Bit32u)(readHandler(addr+0) << 0);
+		ret     |= (readHandler(addr+1) << 8);
+		ret     |= (readHandler(addr+2) << 16);
+		ret     |= (readHandler(addr+3) << 24);
+		return ret;
+	}
+public:
+	void writeHandler(PhysPt start, Bit8u val) {
+        VGA_Generic_Write_Handler<false/*chained*/>(start, start, val);
+	}
+public:
+	VGA_UnchainedVGA_Handler() : PageHandler(PFLAG_NOCODE) {}
+	void writeb(PhysPt addr,Bit8u val) {
+		addr = PAGING_GetPhysicalAddress(addr) & vga.mem.memmask;
+		writeHandler(addr+0,(Bit8u)(val >> 0));
+	}
+	void writew(PhysPt addr,Bit16u val) {
+		addr = PAGING_GetPhysicalAddress(addr) & vga.mem.memmask;
+		writeHandler(addr+0,(Bit8u)(val >> 0));
+		writeHandler(addr+1,(Bit8u)(val >> 8));
+	}
+	void writed(PhysPt addr,Bit32u val) {
+		addr = PAGING_GetPhysicalAddress(addr) & vga.mem.memmask;
+		writeHandler(addr+0,(Bit8u)(val >> 0));
+		writeHandler(addr+1,(Bit8u)(val >> 8));
+		writeHandler(addr+2,(Bit8u)(val >> 16));
+		writeHandler(addr+3,(Bit8u)(val >> 24));
+	}
+};
+
+static struct vg {
+	VGA_UnchainedVGA_Handler	uvga;
+} vgaph;
+
+void VGA_SetupHandlers(void) {
+    MEM_SetPageHandler( VGA_PAGE_B8, 8,  &vgaph.uvga  );
+	PAGING_ClearTLB();
+}
+
+static bool VGA_Memory_ShutDown_init = false;
+
+static void VGA_Memory_ShutDown(Section * /*sec*/) {
+	PAGING_ClearTLB();
+
+	if (vga.mem.linear_orgptr != NULL) {
+		delete[] vga.mem.linear_orgptr;
+		vga.mem.linear_orgptr = NULL;
+		vga.mem.linear = NULL;
+	}
+}
+
+void VGA_SetupMemory() {
+    if (vga.mem.linear == NULL) {
+        VGA_Memory_ShutDown(NULL);
+
+        vga.mem.linear_orgptr = new Bit8u[vga.mem.memsize+32u];
+        memset(vga.mem.linear_orgptr,0,vga.mem.memsize+32u);
+        vga.mem.linear=(Bit8u*)(((uintptr_t)vga.mem.linear_orgptr + 16ull-1ull) & ~(16ull-1ull));
+
+        /* HACK. try to avoid stale pointers */
+	    vga.draw.linear_base = vga.mem.linear;
+
+        /* may be related */
+        VGA_SetupHandlers();
+    }
+
+	if (!VGA_Memory_ShutDown_init) {
+		AddExitFunction(AddExitFunctionFuncPair(VGA_Memory_ShutDown));
+		VGA_Memory_ShutDown_init = true;
+	}
 }
 
