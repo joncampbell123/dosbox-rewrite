a68b309f08fe7f2e42d13d9fecbf3b7296d2f7c7
Jonathan Campbell <jonathan@castus.tv>
2020-03-15 22:40:51 -0700

    more

--- src/hardware/vga.cpp
+++ src/hardware/vga.cpp
@@ -116,7 +116,7 @@ struct VGACRTCDACStatus_Dim {
 
 struct VGACRTCDACStatus_Dim_V : VGACRTCDACStatus_Dim {
     unsigned int                    row_height_mask = 0x1Fu;        // row height mask (register width)
-    unsigned int                    row_height = 0;                 // row height (next line when char_count == row_height)
+    unsigned int                    row_height = 1;                 // row height (next line when char_count == row_height)
     unsigned int                    scanline_double_count = 0;      // scanline doubling in hardware, counter
     unsigned int                    scanline_double_max = 1;        // max
 };
